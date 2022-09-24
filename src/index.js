e96ac5a2f4f206bec42a49251650ff9ccac72eb4
Sumanto Banerjee <sbanerjee@Sumantos-MacBook-Pro.local>
2022-09-24 17:40:35 +0530

    Added UI for posts - error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
 import { configureStore } from './store';
@@ -9,9 +10,11 @@ const store = configureStore();
 console.log('store', store.getState());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
